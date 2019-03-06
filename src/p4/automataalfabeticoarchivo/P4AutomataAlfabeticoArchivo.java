90eed17a0016b6da3e17e91c7a13dc8da5061e91
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-03-06 01:56:09 -0600

    primer commit

---
+++ src/p4/automataalfabeticoarchivo/P4AutomataAlfabeticoArchivo.java
@@ -0,0 +1,20 @@
+package p4.automataalfabeticoarchivo;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P4AutomataAlfabeticoArchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
